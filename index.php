a334f941253a6618c279531b2ac5bacc6bf3080d
miakojason <miakojason@gmail.com>
2023-11-17 11:24:53 +0800

    不定參數的用法

--- index.php
+++ index.php
@@ -1,9 +1,12 @@
+<h1>自訂函式</h1>
 <?php
 //自訂函式
-$c=20;
+$c=20;   
 function sum($a,$b){
+
     // global呼叫外面區域函數
     global $c;
+ 
     $sum=$a+$b+$c;
     echo "輸入:".$a."、".$b;
     echo "<br>";
@@ -13,11 +16,31 @@ function sum($a,$b){
 $sum=sum(10,20);
 echo "總和是：".$sum;
 echo "<hr>";
-$sum=sum(17,22);
+$sum=sum(10,20);
 echo "總和是：".$sum;
 echo "<hr>";
 
-echo "總和是:".sum(56,77);
+echo "總和是:".sum(56,21);
+// echo "總和是2:".sum(77);
+
+?>
+<h1>不定參數的用法</h1>
+<?php
+function sum2(...$arg) {
+    $sum = 0;
+
+    foreach ($arg as $num) {
+        $sum += $num;
+    }
+
+    return $sum;
+}
+echo sum2(1,2);
+echo"<hr>";
+echo sum2(23,45,89);
+echo"<hr>";
+echo sum2(23,98,34,89,22,3,4);
+echo"<hr>";
 
 
 ?>
